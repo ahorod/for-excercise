4ee751ca8c29eed7576e2ab1ac238e9b2459bf4f
Anna horodetska, Bryan Lin and Jonathon Kang <bryan@>
2017-03-28 15:06:54 -0700

    add javascript for prime sieve

--- js/scripts.js
+++ js/scripts.js
@@ -13,36 +13,54 @@ $(document).ready(function(){
 
 
 // Palindromes
+//   $('#palindromeForm').submit(function(event){
+//     var sentence = $('#palindromeInput').val().split(' ').join('').toLowerCase();
+//     var split = [];
+//
+//     if(sentence.length%2===1){
+//       split = sentence.split("");
+//       split.splice(sentence.length/2,1);
+//       console.log('odd', split);
+//     }
+//     else {
+//       split = sentence.split("");
+//         console.log('even');
+//     }
+//
+//     var half = split.slice(split.length/2).reverse();
+//
+//     var isPalindrome = true;
+//     for(i=0;i<half.length;i++){
+//       if(half[i]!==sentence[i]){
+//         isPalindrome = false;
+//         break;
+//       }
+//     }
+//     if(isPalindrome === true){
+//       alert("It's a palindrome.");
+//     }
+//     else{
+//       alert('Nope');
+//     }
+//     event.preventDefault();
+//   });
 
-  $('#palindromeForm').submit(function(event){
-    var sentence = $('#palindromeInput').val().split(' ').join('').toLowerCase();
-    var split = [];
 
-    if(sentence.length%2===1){
-      split = sentence.split("");
-      split.splice(sentence.length/2,1);
-      console.log('odd', split);
-    }
-    else {
-      split = sentence.split("");
-        console.log('even');
-    }
+//Prime Sifting
+var number = 100;
+var numberList = [];
 
-    var half = split.slice(split.length/2).reverse();
+for(i=2;i<=number;i++){
+  numberList.push(i);
+}
 
-    var isPalindrome = true;
-    for(i=0;i<half.length;i++){
-      if(half[i]!==sentence[i]){
-        isPalindrome = false;
-        break;
-      }
+for(j=2;j<=number;j++){
+  for(k=0;k<=numberList.length;k++){
+    if(numberList[k]%j===0 && numberList[k]!=j){
+      numberList.splice(k,1);
     }
-    if(isPalindrome === true){
-      alert("It's a palindrome.");
-    }
-    else{
-      alert('Nope');
-    }
-    event.preventDefault();
-  });
+  }
+}
+console.log(numberList);
+
 });
