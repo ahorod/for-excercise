fd7665950d971531861d44677284a5317919fc4c
Anna horodetska, Bryan Lin and Jonathon Kang <anna@>
2017-03-28 14:07:21 -0700

    add functions and arrays and branching to determine if form input is a palindrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,48 @@
 $(document).ready(function(){
 
 // Factorials
-  $('#factorial').submit(function(event){
-    var factorial = parseInt($('#input').val());
-    var answer = 1;
-    for(i=factorial; i>1; i--){
-      answer *= i;
+  // $('#factorial').submit(function(event){
+  //   var factorial = parseInt($('#input').val());
+  //   var answer = 1;
+  //   for(i=factorial; i>1; i--){
+  //     answer *= i;
+  //   }
+  //   alert(answer);
+  //   event.preventDefault();
+  // });
+
+
+// Palindromes
+
+  $('#palindromeForm').submit(function(event){
+    var sentence = $('#palindromeInput').val().split(' ').join('').toLowerCase();
+    var split = [];
+
+    if(sentence.length%2===1){
+      split = sentence.split("");
+      split.splice(sentence.length/2,1);
+      console.log('odd', split);
     }
-    alert(answer);
+    else {
+      split = sentence.split("");
+        console.log('even');
+    }
+
+    var half = split.slice(split.length/2).reverse();
+
+    var isPalindrome = true;
+    for(i=0;i<half.length;i++){
+      if(half[i]!==sentence[i]){
+        isPalindrome = false;
+        break;
+      }
+    }
+    if(isPalindrome === true){
+      alert("It's a palindrome.");
+    }
+    else{
+      alert('Nope');
+    }
     event.preventDefault();
   });
-
-
-
-
-
-
-
-
 });
